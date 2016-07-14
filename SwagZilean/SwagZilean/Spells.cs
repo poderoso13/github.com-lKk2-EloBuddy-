cebc7444eeb23ba466115307ab50edb508c6f36c
Maauh <mauricio.jr4085@hotmail.com>
2016-07-14 06:19:21 -0300

    Update Spells.cs

--- SwagZilean/SwagZilean/Spells.cs
+++ SwagZilean/SwagZilean/Spells.cs
@@ -12,7 +12,7 @@ namespace SwagZilean
 
         public static void getSpells()
         {
-            Q = new Spell.Skillshot(SpellSlot.Q, 900, SkillShotType.Circular, (int) 280, 1850, 150);
+            Q = new Spell.Skillshot(SpellSlot.Q, 900, SkillShotType.Circular, (int) 0.70f, 2500, 140);
             Q.AllowedCollisionCount = int.MaxValue;
             W = new Spell.Active(SpellSlot.W);
             E = new Spell.Targeted(SpellSlot.E, 700);
